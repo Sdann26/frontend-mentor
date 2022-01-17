2ad222a0674ed0289a4273d0f80a5076b6630c16
Danilo <sdann_26@hotmail.com>
2022-01-17 18:02:01 -0500

    Finish

--- faq-accordion-card-main/index.js
+++ faq-accordion-card-main/index.js
@@ -4,11 +4,18 @@ const icons = Array.from(document.querySelectorAll(".FAQ-accordion__question-img
 
 for (const i in questions) {
     questions[i].addEventListener('click', ()=>{
-        reset(questions);
-        console.log("Clicki");
-        questions[i].classList.add('FAQ-accordion__question--active');
-        answers[i].classList.add('FAQ-accordion__answer--active');
-        icons[i].classList.add('FAQ-accordion__question-img--active');
+        console.log(questions[i].classList.contains('FAQ-accordion__question--active'));
+        if(questions[i].classList.contains('FAQ-accordion__question--active') == true){
+            questions[i].classList.remove('FAQ-accordion__question--active');
+            answers[i].classList.replace('FAQ-accordion__answer--active','accordion__question-img--desactive');
+            icons[i].classList.remove('FAQ-accordion__question-img--active');
+        }
+        else{
+            reset(questions);
+            questions[i].classList.add('FAQ-accordion__question--active');
+            answers[i].classList.add('FAQ-accordion__answer--active');
+            icons[i].classList.add('FAQ-accordion__question-img--active');
+        }
     });
 }
 
@@ -16,8 +23,8 @@ const reset = (arr)=>{
     for (const i in arr) {
         let eliminate = !arr[i].classList.toggle('FAQ-accordion__question--active', false);
         if(eliminate == true){
-            answers[i].classList.replace('FAQ-accordion__answer--active','accordion__question-img--desactive')
-            icons[i].classList.remove('FAQ-accordion__question-img--active')
+            answers[i].classList.replace('FAQ-accordion__answer--active','accordion__question-img--desactive');
+            icons[i].classList.remove('FAQ-accordion__question-img--active');
         }
     }
 }
