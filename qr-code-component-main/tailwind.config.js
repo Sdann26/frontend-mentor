c1aa651b4b765dcf2d0d04d3202d88f69a51cab5
Danilo <sdann_26@hotmail.com>
2022-02-15 18:56:51 -0500

    Finished Project

--- qr-code-component-main/tailwind.config.js
+++ qr-code-component-main/tailwind.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Light-gray': 'hsl(212, 45%, 89%)',
+        'Grayish-blue': 'hsl(220, 15%, 55%)',
+        'Dark-blue': 'hsl(218, 44%, 22%)',
+      },
+    },
   },
   plugins: [],
 }
