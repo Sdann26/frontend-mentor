fa93b697eb14cea4e14914f1c2765eb9bc264a92
Danilo <sdann_26@hotmail.com>
2022-02-20 13:39:41 -0500

    JS

--- advice-generator-app-main/scripts/index.js
+++ advice-generator-app-main/scripts/index.js
@@ -1,1 +1,43 @@
 "use strict";
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+var button = document.getElementById("generator-button");
+var idquote = document.getElementById("id-quote");
+var quote = document.getElementById("quote");
+var rspt;
+
+var generatedSentence = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.next = 2;
+            return fetch('https://api.adviceslip.com/advice').then(function (response) {
+              return response.json();
+            }).then(function (data) {
+              return data.slip;
+            });
+
+          case 2:
+            rspt = _context.sent;
+            idquote.textContent = rspt.id;
+            quote.textContent = "\u201C".concat(rspt.advice, "\u201D");
+
+          case 5:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function generatedSentence() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+button.addEventListener('click', generatedSentence);
---
+++ advice-generator-app-main/scripts/main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const button = document.getElementById("generator-button")
+const idquote = document.getElementById("id-quote")
+const quote = document.getElementById("quote")
+let rspt;
+
+const generatedSentence = async ()=>{
+  rspt = await fetch('https://api.adviceslip.com/advice')
+              .then(response => response.json())
+              .then(data => data.slip)
+  idquote.textContent = rspt.id
+  quote.textContent = `“${rspt.advice}”`
+}
+
+button.addEventListener('click', generatedSentence)
