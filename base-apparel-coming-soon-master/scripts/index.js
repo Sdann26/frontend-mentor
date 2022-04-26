1c7e196350e7a405b0096b1d8ad386256a9508a8
Danilo <sdann_26@hotmail.com>
2022-04-26 18:43:30 -0500

    Finish

--- base-apparel-coming-soon-master/scripts/index.js
+++ base-apparel-coming-soon-master/scripts/index.js
@@ -9,6 +9,11 @@ const formValidation = {
 };
 
 const validation = () => {
+  if (formValidation.email === false) {
+    input.classList.add("form__input--error");
+    errorText[0].classList.add("form__error--visible");
+    errorIcon[0].classList.add("form__error-icon--visible");
+  }
   return Object.values(formValidation).every((e) => e === true) ? true : false;
 };
 
@@ -16,17 +21,15 @@ input.addEventListener("change", (e) => {
   let emailRegex =
     /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
-  if (emailRegex.test(e.target.value)) {
-    formValidation.email = true;
-    input.classList.remove("form__input--error");
-    errorText[0].classList.remove("form__error--visible");
-    errorIcon[0].classList.remove("form__error-icon--visible");
-  } else {
-    formValidation.email = false;
-    input.classList.add("form__input--error");
-    errorText[0].classList.add("form__error--visible");
-    errorIcon[0].classList.add("form__error-icon--visible");
-  }
+  emailRegex.test(e.target.value)
+    ? (formValidation.email = true)
+    : (formValidation.email = false);
+});
+
+input.addEventListener("input", () => {
+  input.classList.remove("form__input--error");
+  errorText[0].classList.remove("form__error--visible");
+  errorIcon[0].classList.remove("form__error-icon--visible");
 });
 
 form.addEventListener("submit", (e) => {
--- base-apparel-coming-soon-master/scripts/main.js
+++ base-apparel-coming-soon-master/scripts/main.js
@@ -10,6 +10,12 @@ var formValidation = {
 };
 
 var validation = function validation() {
+  if (formValidation.email === false) {
+    input.classList.add("form__input--error");
+    errorText[0].classList.add("form__error--visible");
+    errorIcon[0].classList.add("form__error-icon--visible");
+  }
+
   return Object.values(formValidation).every(function (e) {
     return e === true;
   }) ? true : false;
@@ -17,18 +23,12 @@ var validation = function validation() {
 
 input.addEventListener("change", function (e) {
   var emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-
-  if (emailRegex.test(e.target.value)) {
-    formValidation.email = true;
-    input.classList.remove("form__input--error");
-    errorText[0].classList.remove("form__error--visible");
-    errorIcon[0].classList.remove("form__error-icon--visible");
-  } else {
-    formValidation.email = false;
-    input.classList.add("form__input--error");
-    errorText[0].classList.add("form__error--visible");
-    errorIcon[0].classList.add("form__error-icon--visible");
-  }
+  emailRegex.test(e.target.value) ? formValidation.email = true : formValidation.email = false;
+});
+input.addEventListener("input", function () {
+  input.classList.remove("form__input--error");
+  errorText[0].classList.remove("form__error--visible");
+  errorIcon[0].classList.remove("form__error-icon--visible");
 });
 form.addEventListener("submit", function (e) {
   e.preventDefault();
