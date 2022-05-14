120c97a54c0b6ffb24b7370e9ec5d03b88dd8820
Danilo <sdann_26@hotmail.com>
2022-05-14 00:56:57 -0500

    Details

--- qr-code-component-main/tailwind.config.js
+++ qr-code-component-main/tailwind.config.js
@@ -3,11 +3,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'Light-gray': 'hsl(212, 45%, 89%)',
-        'Grayish-blue': 'hsl(220, 15%, 55%)',
-        'Dark-blue': 'hsl(218, 44%, 22%)',
+        "Light-gray": "hsl(212, 45%, 89%)",
+        "Grayish-blue": "hsl(220, 15%, 55%)",
+        "Dark-blue": "hsl(218, 44%, 22%)",
       },
+    },
+    fontFamily: {
+      outfit: ["Outfit", "'sans-serif'"],
     },
   },
   plugins: [],
-}
+};
